5d70a8deb5b998c74dc375abe1149cf9036f5b8a
shivambirla784 <86475649+shivambirla784@users.noreply.github.com>
2024-09-21 23:28:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+
+codes.forEach((code, index) => {
+    code.addEventListener('keydown', (e) => {
+        if (e.key >= 0 && e.key <= 9) {
+            codes[index].value = '';  // Clear the current input
+            setTimeout(() => codes[index + 1]?.focus(), 10);  // Focus the next input after typing
+        } else if (e.key === 'Backspace') {
+            setTimeout(() => codes[index - 1]?.focus(), 10);  // Focus the previous input on backspace
+        }
+    });
+});
